398ed77ee89c9fef07391cee17236cc011b1dbc6
sivertism <sivert.sliper@lyse.net>
2016-03-22 09:07:35 +0100

    Added VESC interface functions.

--- inc/rov2016_Interface.h
+++ inc/rov2016_Interface.h
@@ -1,17 +1,41 @@
 /**
   **************************************************************************************
-  * @file    rov2016_Interface.h
-  * @author  Sivert Sliper, Stian Soerensen
-  * @version V1.0
-  * @date    3-February-2016
-  * @brief   This file contains local variables and macros for rov2016_Interface.c
+  * @file    	rov2016_Interface.h
+  * @author  	Sivert Sliper, Stian Soerensen
+  * @version 	V1.0
+  * @date   	3-February-2016
+  * @brief   	This file contains macros and extern function prototypes for
+  * 			rov2016_Interface.c
   **************************************************************************************
   */
+
+/* VESC interface. */
+#define VESC_DUTY_CYCLE_MAX						0.95f
+#define VESC_DUTY_CYCLE_MIN						-0.95f
+#define VESC_DUTY_CYCLE_DIR_CHANGE				0.3f
+
+#define	ESC_ID_1								1
+#define	ESC_ID_2								2
+#define	ESC_ID_3								3
+#define	ESC_ID_4								4
+#define	ESC_ID_5								5
+#define	ESC_ID_6								6
+#define	ESC_ID_7								7
+#define	ESC_ID_8								8
+#define	ESC_ID_9								9
+#define	ESC_ID_10								10
+#define	ESC_ID_11								11
+#define	ESC_ID_12								12
 
 /* Exported function prototypes --------------------------------------------------------*/
 uint16_t ADC1_getChannel(uint8_t channel);
 uint16_t ADC4_getChannel(uint8_t channel);
+
+/* CANBUS to topside. */
 extern void CAN_transmitQuaternions(int16_t q0, int16_t q1, int16_t q2, int16_t q3);
 extern void CAN_transmitAcceleration(uint8_t* acc_array);
 extern void CAN_transmit_AN_RAW(void);
 extern void CAN_transmitAlive(void);
+
+/* VESC interface */
+VESC_setDutyCycle(uint8_t esc_id, float duty)
--- src/rov2016_Interface.c
+++ src/rov2016_Interface.c
@@ -22,6 +22,20 @@
 /* Private variables -------------------------------------------------------------------*/
 static uint8_t dataBuffer[8] = {0};
 static uint8_t counter_alive = 0;
+
+typedef enum {
+	CAN_PACKET_SET_DUTY = 0,
+	CAN_PACKET_SET_CURRENT,
+	CAN_PACKET_SET_CURRENT_BRAKE,
+	CAN_PACKET_SET_RPM,
+	CAN_PACKET_SET_POS,
+	CAN_PACKET_FILL_RX_BUFFER,
+	CAN_PACKET_FILL_RX_BUFFER_LONG,
+	CAN_PACKET_PROCESS_RX_BUFFER,
+	CAN_PACKET_PROCESS_SHORT_BUFFER,
+	CAN_PACKET_STATUS
+} CAN_PACKET_ID;
+
 /* Function definitions ----------------------------------------------------------------*/
 
 /**
@@ -95,4 +109,36 @@ extern void CAN_transmitAHRS(int16_t pitch, int16_t roll, int16_t yaw, uint16_t 
 	dataBuffer[7] = (uint8_t)(heading & 0xFF);
 
 	CAN_transmitBuffer(SENSOR_AHRS, dataBuffer, 8, CAN_ID_STD);
+}
+
+/**
+ * @brief  	Sets the duty cycle of the specified VESC BLDC controller.
+ * @param  	esc_id:	Can be a value of ESC_ID_x where x can be 1-12.
+ * 			duty:	Duty cycle, can be a value between DUTY_CYCLE_MIN and
+ * 					DUTY_CYCLE_MAX.
+ * @retval 	None
+ */
+void VESC_setDutyCycle(uint8_t esc_id, float duty){
+	/* Regn ut ID
+	 * skaler float x 10 000
+	 * legg float inn i 4 byte array
+	 * send med metoden can_transmitbuffer.
+	 */
+
+	/* Check parameters. */
+	if (duty < VESC_DUTY_CYCLE_MIN) return;
+	if (duty > VESC_DUTY_CYCLE_MAX) return;
+	if(esc_id > 255) return;
+
+	uint32_t id = (uint32_t)(CAN_PACKET_SET_DUTY << 8) | esc_id;
+
+	uint32_t temp_duty = (uint32_t)(duty * 10000.0f);
+
+	uint8_t buffer[4];
+	buffer[0] = temp_duty >> 24;	// MSB(Most significant byte).
+	buffer[1] = temp_duty >> 16;
+	buffer[2] = temp_duty >> 8;
+	buffer[3] = temp_duty;			// LSB(Least significant byte).
+
+	CAN_transmitBuffer(id, buffer, 4, CAN_ID_TYPE_EXT);
 }
